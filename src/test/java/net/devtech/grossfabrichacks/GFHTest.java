fb0dc4eb8b75800b2adc470ceac5278914eb0f5e
user11681 <username11681@gmail.com>
2020-10-11 09:24:07 +0100

    update dependency

--- src/test/java/net/devtech/grossfabrichacks/GFHTest.java
+++ src/test/java/net/devtech/grossfabrichacks/GFHTest.java
@@ -1,8 +1,5 @@
 package net.devtech.grossfabrichacks;
 
-import java.util.Arrays;
-import net.devtech.grossfabrichacks.instrumentation.InstrumentationApi;
-import net.devtech.grossfabrichacks.unsafe.UnsafeUtil;
 import net.gudenau.lib.unsafe.Unsafe;
 import net.minecraft.block.Block;
 import org.junit.jupiter.api.Test;
@@ -16,21 +13,6 @@ class GFHTest {
     static final int iterations = 5000;
 
     @Test
-    void unsafeUtilTest() throws Throwable {
-        final byte[] bytes = {0, 1, 2, 3};
-
-        System.out.println(bytes);
-        System.out.println(Arrays.toString(bytes));
-
-        UnsafeUtil.upcastArray(bytes, UnsafeUtil.INT_ARR_KLASS, 4);
-
-        final int[] ints = (int[]) (Object) bytes;
-
-        System.out.println(ints);
-        System.out.println(Arrays.toString(ints));
-    }
-
-    @Test
     void test() throws Throwable {
         final Object[] objects = {new GFHTest(), Unsafe.allocateInstance(Block.class)};
         final int offset = Unsafe.arrayBaseOffset(objects.getClass());
@@ -40,13 +22,5 @@ class GFHTest {
 
         System.out.println(Long.toHexString(address0));
         System.out.println(Long.toHexString(address1));
-    }
-
-    void printValues(final Object object) throws Throwable {
-        for (int i = 0; i < InstrumentationApi.instrumentation.getObjectSize(object); i += 4) {
-            System.out.printf("%08X ", Unsafe.getInt(object, i));
-        }
-
-        System.out.println();
     }
 }
